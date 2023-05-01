9559d02b897ca1eaf3122f68318b7605060068d3
Edmund Ju <edmundju@berkeley.edu>
2023-05-01 11:15:08 -0700

    remove footer stars and forks

--- src/components/footer.js
+++ src/components/footer.js
@@ -108,7 +108,7 @@ const Footer = () => {
         <a href="https://github.com/bchiang7/v4">
           <div>Thank you, Brittany Chiang, for this open-source project!</div>
 
-          {githubInfo.stars && githubInfo.forks && (
+          {/* {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
               <span>
                 <Icon name="Star" />
@@ -119,7 +119,7 @@ const Footer = () => {
                 <span>{githubInfo.forks.toLocaleString()}</span>
               </span>
             </div>
-          )}
+          )} */}
         </a>
       </StyledCredit>
     </StyledFooter>
